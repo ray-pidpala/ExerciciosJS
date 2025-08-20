c6aac583f256a70bbadd57df2ff6ebd551bb5a4c
ray-pidpala <rayane.pidpala.ifpr.2023@gmail.com>
2025-08-19 21:04:28 -0300

    Delete Aulas_JS/Aulas_JS/Aulas_JS directory

--- Aulas_JS/Aulas_JS/Aulas_JS/js/ex3.js
+++
@@ -1,4 +0,0 @@
-var n1,r
-n1=parseInt(prompt("Digite o valor desejado: "))
-r=n1*n1*n1
-alert("O resultado é: "+r)
--- Aulas_JS/Aulas_JS/Aulas_JS/js/ex8.js
+++
@@ -1,7 +0,0 @@
-var n1,n2,n3,n4,r
-n1=parseInt(prompt("Insira o valor da primeira nota: "))
-n2=parseInt(prompt("Insira o valor da segunda nota: "))
-n3=parseInt(prompt("Insira o valor da terceira nota: "))
-n4=parseInt(prompt("Insira o valor da quarta nota: "))
-r=(n1+n2+n3+n4)/4
-alert("A média anual é: "+r)
