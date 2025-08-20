e64e99ffcabe7fac8306f7f212dcf3c0c8c632a5
ray-pidpala <rayane.pidpala.ifpr.2023@gmail.com>
2025-08-19 22:04:22 -0300

    Delete js directory

--- js/ex2.js
+++
@@ -1,6 +0,0 @@
-var n1,n2,n3,r
-n1=parseInt(prompt("Digite o primeiro valor: "))
-n2=parseInt(prompt("Digite o segundo valor: "))
-n3=parseInt(prompt("Digite o terceiro valor: "))
-r=n1*n2*n3
-alert("O resultado é: "+r)
--- js/ex6.js
+++
@@ -1,5 +0,0 @@
-var km,l,r
-km=parseInt(prompt("Insira a quantidade de km percorrido: "))
-l=parseInt(prompt("Insira a quantidade de litros gastos: "))
-r=km/l
-alert("A média de km/l é: "+r)
